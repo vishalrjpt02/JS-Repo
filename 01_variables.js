f46711a86784fa2614430435ac59bdf782eeb065
vishalrjpt02 <55855196+vishalrjpt02@users.noreply.github.com>
2023-12-23 17:35:49 +0000

    data tpes and conversions

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId =114553
+let accoountEmail = "vishalrjpt01@google.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+accountSate = "karnataka"
+
+//accountId = 2 not allowed
+
+accoountEmail = "visha@vs.com"
+accountPassword = "212321"
+accountCity = "bangluru"
+
+//console.log(accountId)
+
+/*
+Prefer not usee "var" coz it has issue of block SCOPE/funtional scope ie:{} [] ()
+*/
+
+console.table ([accountId,accoountEmail,accountPassword,accountCity,accountSate])
---
+++ 03_conversionOpration.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber =Number(score)
+//console.log(typeof score)
+//console.log(valueInNumber) //NAN coz it has abc with number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIN = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIN)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
