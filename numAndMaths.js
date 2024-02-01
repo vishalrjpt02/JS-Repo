09a535eeb8c31cee82d8851d9f4cdd1da28ce389
vishalrjpt02 <55855196+vishalrjpt02@users.noreply.github.com>
2024-02-01 02:31:24 +0000

    maths logic added

---
+++ numAndMaths.js
@@ -0,0 +1,15 @@
+const score=400;
+const balance =new Number(100);
+console.log(balance);
+
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(1));
+
+const ohterNumber =1123.00003
+console.log(ohterNumber.toPrecision(3))
+
+const hundreds =100000
+console.log(hundreds.toLocaleString(en-IN))
