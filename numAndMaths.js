dbd04595c43e26358f2f45f4959145a6b3304ac7
vishalrjpt02 <55855196+vishalrjpt02@users.noreply.github.com>
2024-02-01 03:03:14 +0000

    Math and num final commit

--- numAndMaths.js
+++ numAndMaths.js
@@ -3,13 +3,42 @@ const balance =new Number(100);
 console.log(balance);
 
 
-console.log(balance.toString());
-console.log(balance.toString().length);
+//console.log(balance.toString());
+//console.log(balance.toString().length);
 
-console.log(balance.toFixed(1));
+//console.log(balance.toFixed(1));
 
 const ohterNumber =1123.00003
-console.log(ohterNumber.toPrecision(3))
+//console.log(ohterNumber.toPrecision(3))
 
-const hundreds =100000
-console.log(hundreds.toLocaleString(en-IN))
+//console.log(ohterNumber.toPrecision(4))
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+//++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.assert(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.2));
+// console.log (Math.floor(4.9))
+
+// console.log (Math.min(4,3,6,8));
+
+// console.log (Math.max(4,3,6,8));
+
+
+console.log(Math.random()); //return b/w 0 to 1
+console.log((Math.random()*10)+1); // 1 to 9
+
+console.log(Math.floor((Math.random()*10))+1);
+
+const min = 10
+const max = 20
+
+
+// given a range and added amin value 
+console.log(Math.random() * (max -min +1) + min)  
