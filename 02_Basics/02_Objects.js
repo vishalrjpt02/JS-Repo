3acaf447eaba2d0f701bbae3bf90f6b66f01ea95
vishalrjpt02 <55855196+vishalrjpt02@users.noreply.github.com>
2024-02-02 18:09:57 +0000

    finalobject 2

--- 02_Basics/02_Objects.js
+++ 02_Basics/02_Objects.js
@@ -4,13 +4,50 @@
 //Object Literals
 
 //object literls
+
+const mySym = Symbol("Key1")
+
 const Juser ={
     name : "Vishal",
     age :19,
+    [mySym] : "mykey1",
     Location : "Delhi",
     email : "vishal.google.com",
     isLoggedIn : false,
-    lsatLoginDays : "Monday"
+    lsatLoginDays : ["Monday","Tuesday"]
 }
 
-console.log(Juser.email) //
+Juser.email ='vishal@adobe.com'
+
+// console.log(Juser.email) //
+// console.log(Juser["email"]) //
+// console.log(Juser["name"]) // in some sepcial cases []
+// console.log(Juser[mySym])    // forKeys nad symbols no need for ".." this
+
+///Freeze keyword 
+//Object.freeze(Juser) ; // now the value of on=bject can't be changed
+Juser.email ='vishal.paytm.com'
+
+// console.log(Juser)
+
+/*{
+    name: 'Vishal',
+    age: 19,
+    Location: 'Delhi',
+    email: 'vishal@adobe.com',
+    isLoggedIn: false,
+    lsatLoginDays: [ 'Monday', 'Tuesday' ],
+    [Symbol(Key1)]: 'mykey1'
+  }
+*/
+
+
+Juser.gretting = function(){
+    console.log("helllo js user")
+}
+Juser.grettingtwo = function(){
+    console.log(`helllo js user ${this.name}`)
+}
+
+console.log(Juser.gretting());
+console.log(Juser.grettingtwo());
