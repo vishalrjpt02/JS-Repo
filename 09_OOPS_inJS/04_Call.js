6696d0b2140c5d88f56e114f13c19bd3a4f6ab30
Vishal Rajput <vishalrjpt02@outlook.in>
2024-02-19 14:00:54 +0530

    calls update

--- 09_OOPS_inJS/04_Call.js
+++ 09_OOPS_inJS/04_Call.js
@@ -6,8 +6,30 @@ function SetUserName(username) {
 
 function createUser(username, email, password) {
        
+
+    //if we don't user this keyword the username will be undefined
+    /*
+       o/p-------->>
+        called
+        createUser { password: 'chai@fb.com', email: 'chai@fb.com' }
+
+    */
+    //coz funtion will be removed from call stack and values will be destroyed
+
+    // Calls a method of an object, substituting another object for the current object.
     SetUserName.call(this, username);
     
+/*   //Console output....
+
+        called
+        createUser {
+            username: 'chai',
+            password: 'chai@fb.com',
+            email: 'chai@fb.com'
+            }
+            
+*/
+
     this.password = email;
     this.email = email;
     
